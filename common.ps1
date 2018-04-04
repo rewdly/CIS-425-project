94b942dd3338c06027b217b2c0b48dbd508d2efc
vxtang <37963548+vxtang@users.noreply.github.com>
2018-04-04 15:40:23 -0700

    Add files via upload

--- common.ps1
+++ common.ps1
@@ -1,25 +1,3 @@
-function Get-Checksum($file) {
-    $cryptoProvider = New-Object "System.Security.Cryptography.MD5CryptoServiceProvider"
-	
-    $fileInfo = Get-Item $file
-	trap { ;
-	continue } $stream = $fileInfo.OpenRead()
-    if ($? -eq $false) {
-		# Couldn't open file for reading
-        return $null
-	}
-    
-    $bytes = $cryptoProvider.ComputeHash($stream)
-    $checksum = ''
-	foreach ($byte in $bytes) {
-		$checksum += $byte.ToString('x2')
-	}
-    
-	$stream.Close() | Out-Null
-    
-    return $checksum
-}
-
 function AddOrUpdate-Reference($scriptsFolderProjectItem, $fileNamePattern, $newFileName) {
     try {
         $referencesFileProjectItem = $scriptsFolderProjectItem.ProjectItems.Item("_references.js")
@@ -79,32 +57,12 @@ function Remove-Reference($scriptsFolderProjectItem, $fileNamePattern) {
     }
 }
 
-function Delete-ProjectItem($item) {
-    $itemDeleted = $false
-    for ($1=1; $i -le 5; $i++) {
-        try {
-            $item.Delete()
-            $itemDeleted = $true
-            break
-        }
-        catch {
-            # Try again in 200ms
-            [System.Threading.Thread]::Sleep(200)
-        }
-    }
-    if ($itemDeleted -eq $false) {
-        throw "Unable to delete project item after five attempts."
-    }
-}
-
-# Extract the version number from the jquery file in the package's content\scripts folder
+# Extract the version number from the file in the package's content\scripts folder
 $packageScriptsFolder = Join-Path $installPath Content\Scripts
-$jqueryFileName = Join-Path $packageScriptsFolder "jquery-*.js" | Get-ChildItem -Exclude "*.min.js","*-vsdoc.js" | Split-Path -Leaf
-$jqueryFileNameRegEx = "jquery-((?:\d+\.)?(?:\d+\.)?(?:\d+\.)?(?:\d+)).js"
-$jqueryFileName -match $jqueryFileNameRegEx
+$modernizrFileName = Join-Path $packageScriptsFolder "modernizr-*.js" | Get-ChildItem -Exclude "*.min.js","*-vsdoc.js" | Split-Path -Leaf
+$modernizrFileNameRegEx = "modernizr-((?:\d+\.)?(?:\d+\.)?(?:\d+\.)?(?:\d+)).js"
+$modernizrFileName -match $modernizrFileNameRegEx
 $ver = $matches[1]
-
-$intelliSenseFileName = "jquery-$ver.intellisense.js"
 
 # Get the project item for the scripts folder
 try {
